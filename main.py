9a7bea1260adc25777a56a19e60f94149d287c03
Marina Lobina <mvlobina@gmsil.com>
2021-02-12 01:13:27 +0300

    final

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import requests
 import json
 import os
+import hashlib
 
 link = 'https://raw.githubusercontent.com/mledoze/countries/master/countries.json'
 
 class SearchEngine:
 
-    def __init__(self):
+    def __init__(self, link):
         self.link = link
 
     def download_file(self, link):
@@ -18,26 +19,28 @@ class SearchEngine:
             return file_name
 
     def get_countries_data(self, file_name):
+        self.file_name = file_name
         with open(file_name, 'rb') as file:
             countries_data = json.load(file)
             return countries_data
 
     def get_wiki_links(self, countries_data):
+        self.countries_data = countries_data
 
-        if os.path.isfile('coutries_wiki_links.txt') == True:
-            os.remove('coutries_wiki_links.txt')
+        if os.path.isfile('countries_wiki_links.txt') == True:
+            os.remove('countries_wiki_links.txt')
         else:
             pass
         for item in countries_data:
             country = item['name']['official']
-            with open('coutries_wiki_links.txt', 'a') as file:
+            with open('countries_wiki_links.txt', 'a') as file:
                 file.write(f'{country} - "https://en.wikipedia.org/wiki/{country}", \n')
 
 
     def main(self):
         self.get_wiki_links(self.get_countries_data(self.download_file(link)))
 
-wiki_searcher = SearchEngine()
+wiki_searcher = SearchEngine(link)
 
 wiki_searcher.main()
 
