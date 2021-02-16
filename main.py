5185bfa8631260d4ad63eee546ae18cf652ea48e
Marina Lobina <mvlobina@gmsil.com>
2021-02-17 01:03:39 +0300

    class Iterator,class Generator

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import os
-
+import hashlib
 
 link = 'https://raw.githubusercontent.com/mledoze/countries/master/countries.json'
 
@@ -25,7 +25,6 @@ class SearchEngine:
             return data
 
 searcher1 = SearchEngine(link)
-
 data = searcher1.get_countries_data(searcher1.download_file())
 
 class Iterator:
@@ -55,5 +54,29 @@ class Iterator:
 
 
 iterator1 = Iterator()
-
 iterator1.print_file()
+
+
+class Generator:
+
+    def __init__(self, file_name='countries_wiki_links.txt'):
+        self.file_name = file_name
+
+    def get_hashsum(self):
+        file_name = self.file_name
+
+        with open(file_name) as f:
+            for line in f:
+                yield hashlib.md5(line.encode())
+
+    def print_file(self):
+        if os.path.isfile('hashsum.txt') == True:
+            os.remove('hashsum.txt')
+        else:
+            pass
+        for item in self.get_hashsum():
+            with open('hashsum.txt', 'a') as f:
+                f.write(f'{item.hexdigest()}, \n')
+
+generator1 = Generator()
+generator1.print_file()
