fda8c6f754c109bf3b8ce076cb3942fc5a1411a0
Marina Lobina <mvlobina@gmsil.com>
2021-02-16 22:50:57 +0300

    class Iterator

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ class SearchEngine:
     def __init__(self, link):
         self.link = link
 
-    def download_file(self, link):
-        link = self.link
-        file_name = link.split('/')[-1]
-        resp = requests.get(link, allow_redirects=True)
+    def download_file(self):
+
+        file_name = self.link.split('/')[-1]
+        resp = requests.get(self.link, allow_redirects=True)
         with open(file_name, 'wb') as file:
             file.write(resp.content)
             return file_name
@@ -21,26 +21,39 @@ class SearchEngine:
     def get_countries_data(self, file_name):
         self.file_name = file_name
         with open(file_name, 'rb') as file:
-            countries_data = json.load(file)
-            return countries_data
+            data = json.load(file)
+            return data
 
-    def get_wiki_links(self, countries_data):
-        self.countries_data = countries_data
+searcher1 = SearchEngine(link)
 
+data = searcher1.get_countries_data(searcher1.download_file())
+
+class Iterator:
+
+    def __init__(self, countries=data, start=-1):
+        self.countries = countries
+        self.start = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start < (len(self.countries)-1):
+            self.start += 1
+            country = self.countries[self.start]['name']['official']
+            return country
+        raise StopIteration
+
+    def print_file(self):
         if os.path.isfile('countries_wiki_links.txt') == True:
             os.remove('countries_wiki_links.txt')
         else:
             pass
-        for item in countries_data:
-            country = item['name']['official']
+        for country in Iterator():
             with open('countries_wiki_links.txt', 'a') as file:
                 file.write(f'{country} - "https://en.wikipedia.org/wiki/{country}", \n')
 
 
-    def main(self):
-        self.get_wiki_links(self.get_countries_data(self.download_file(link)))
+iterator1 = Iterator()
 
-wiki_searcher = SearchEngine(link)
-
-wiki_searcher.main()
-
+iterator1.print_file()
